0c11c2b7d6110fb7c508613b4cd44b6eda0d809d
Infiniwild <jsgamache@gmail.com>
2019-06-13 03:25:31 +0000

    Basic settup and module linking

---
+++ app.js
@@ -0,0 +1,27 @@
+// included modules
+const express = require("express"),
+	  app = express(),
+	  bodyParser = require("body-parser"),
+	  mongoose = require("mongoose");
+
+// module activation and linking
+mongoose.connect("mongodb://localhost:27017/wdb_blog", { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+// Schema and model
+
+// INDEX Overall blog page
+// SHOW Shows more info about a single blog post
+// NEW Form to add a new blog post
+// CREATE Add blog post
+// EDIT Form to edit an existing post
+// UPDATE Change the existing post
+// DESTROY Delete the existing post
+
+
+
+// Server start!
+app.listen(3000, function(){
+	console.log("The blog app server has started!");
+});
