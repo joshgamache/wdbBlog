127a0cf990a198e2df9cf88779c660d8cb7b9d74
Josh Gamache <jsgamache@gmail.com>
2019-06-14 03:21:47 +0000

    adding MongoDB atlas?

--- app.js
+++ app.js
@@ -6,10 +6,21 @@ const express = require("express"),
 
 // module activation and linking
 // mongoose.connect("mongodb://localhost:27017/wdb_blog", { useNewUrlParser: true });
-// Set up MongoDB/mongoos using ATLAS to make it server-independent
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+const mongoURL = "mongodb+srv://devidle:" + process.env.MDBauth + "@cluster0-jcmtm.mongodb.net/test?retryWrites=true&w=majority";
+
+// Set up MongoDB/mongoos using ATLAS to make it server-independent
+mongoose.connect(mongoURL, 	useNewUrlParser: true,
+	useCreateIndex: true
+}).then(() => {
+	console.log('Connected to DB!');
+}).catch(err => {
+	console.log('ERROR:', err.message);
+});
+
 
 // Schema and model
 
