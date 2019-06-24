e450e362869d7f7b56982ee4447b9a7a1a243d94
Josh Gamache <jsgamache@gmail.com>
2019-06-24 18:29:57 +0000

    DESTROY route added, buttons updated to match

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, dbName: "wdbBlog", useFindAn
 	console.log('ERROR:', err.message);
 });
 
-// override with POST having ?_method=PUT
+// override with POST having ?_method=PUT or ?_method=DELETE
 app.use(methodOverride('_method'));
 
 // Schema and model
@@ -126,7 +126,7 @@ app.put("/blog/:id", (req, res) => {
 });
 
 // DESTROY Delete the existing post
-app.delete("/blog/:id/delete", (req, res) => {
+app.delete("/blog/:id", (req, res) => {
     Blog.findByIdAndDelete(req.params.id, (err) =>{
         if (err){
             console.log(err);
